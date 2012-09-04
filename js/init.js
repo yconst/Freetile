2cded65495a5949ffab7ae398b22705f0204e65a
Yiannis Chatzikonstantinou <yconst@gmail.com>
2012-09-05 01:25:14 +0200

    Added more tests.

--- js/init.js
+++ js/init.js
@@ -1,13 +1,32 @@
 ;(function($){
 	$(document).ready(function() {
+
+		// First test
 		for (i=0;i<40;i++) {
-			var w = 96 * (parseInt(Math.random() * 2) + 1) - 2,
-				h = 48 * (parseInt(Math.random() * 3) + 1) - 2;
-			$('<div class="element" style="color:#fff;"></div>').width(w).height(h).appendTo('#freetile-demo');
+			var w = 64 * (parseInt(Math.random() * 3) + 1) - 1,
+				h = 48 * (parseInt(Math.random() * 3) + 1) - 1;
+			$('<div class="element" style="color:#fff;"></div>').width(w).height(h).appendTo('.first.test');
 		}
-		$('#freetile-demo').freetile({
-			animate: true,
-			elementDelay: 10
+
+		for (i=0;i<1;i++) {
+			var w = 64 * (parseInt(Math.random() * 3) + 1) - 1,
+				h = 48 * (parseInt(Math.random() * 3) + 1) - 1;
+			$('<div class="element" style="color:#fff;"></div>').width(w).height(h).appendTo('.second.test');
+		}
+
+		for (i=0;i<40;i++) {
+			var w = 64 * (parseInt(Math.random() * 3) + 1) - 1,
+				h = 48 * (parseInt(Math.random() * 3) + 1) - 1;
+			$('<div class="element" style="color:#fff;"></div>').width(w).height(h).appendTo('.third.test');
+		}
+
+
+		$('#freetile-demo').children().each(function()
+		{
+			$(this).freetile({
+				animate: true,
+				elementDelay: 10
+			});
 		});
 	});
 })(jQuery)
