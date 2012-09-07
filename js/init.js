a527314504d3641fb5e4b7d3b657721544f9ee44
Yiannis Chatzikonstantinou <yconst@gmail.com>
2012-09-07 02:06:02 +0200

    Added 2k items test.

--- js/init.js
+++ js/init.js
@@ -14,10 +14,16 @@
 			$('<div class="element" style="color:#fff;"></div>').width(w).height(h).appendTo('.second.test');
 		}
 
-		for (i=0;i<40;i++) {
-			var w = 64 * (parseInt(Math.random() * 3) + 1) - 1,
-				h = 48 * (parseInt(Math.random() * 3) + 1) - 1;
+		for (i=0;i<20;i++) {
+			var w = 64 * (parseInt(Math.random() * 4) + 1) - 1,
+				h = 48 * (parseInt(Math.random() * 4) + 1) - 1;
 			$('<div class="element" style="color:#fff;"></div>').width(w).height(h).appendTo('.third.test');
+		}
+
+		for (i=0;i<2000;i++) {
+			var w = 4 * (parseInt(Math.random() * 9) + 1) - 1,
+				h = 3 * (parseInt(Math.random() * 9) + 1) - 1;
+			$('<div class="element" style="color:#fff;"></div>').width(w).height(h).appendTo('.fourth.test');
 		}
 
 
